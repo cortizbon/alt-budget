bdc2796bb0a1ed2f33946c7354d662a230ccc353
Carlos José Ortiz Bonill <carlosortiz@MacBook-Air-de-Carlos.local>
2024-05-03 15:07:02 -0500

    first push

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout='wide')
+
+st.title("Presupesto alternativo")
+
+
+df = pd.read_csv('datos_desag_2024_cuentas.csv')
+
+df = df[df['Tipo de gasto'] == 'Funcionamiento']
+sectors = list(df['Sector'].unique())
+piv = df.groupby(['Sector'])['Aporte nacional'].sum().sort_values(ascending=False)
+st.dataframe(piv)
+piv[sectors[0]]
+
+dict = {}
+
+for idx, sector in enumerate(sectors):
+    dict[sector] = st.slider(sector, 
+                    min_value=0,
+                    max_value=int(piv[sector]), 
+                    key=idx)
+    
+val = sum(dict.values())
+
+st.dataframe(pd.Series(dict).to_frame())
+
+
+
+
