4464c3b1b41ec215238ebe4b6307790280588a01
Carlos José Ortiz Bonill <carlosortiz@MacBook-Air-de-Carlos.local>
2024-05-03 16:18:31 -0500

    new update

--- app.py
+++ app.py
@@ -34,7 +34,14 @@ for col in [col1, col2, col3]:
                     for idx3, cuenta in enumerate(cuentas):
                             
                         #try:
-                        if int(piv[2019][sector][entidad][cuenta]) >= int(piv[2024][sector][entidad][cuenta]):
+                        if int(piv[2019][sector][entidad][cuenta]) + int(piv[2024][sector][entidad][cuenta]) == 0:
+                            valor = st.slider(f"{sector[:1]}-{entidad[:1]}-{cuenta}", 
+                                                    min_value=0,
+                                                    max_value=100, 
+                                                    key=contador,
+                                                    value=int(piv[2024][sector][entidad][cuenta]))
+                        
+                        elif int(piv[2019][sector][entidad][cuenta]) >= int(piv[2024][sector][entidad][cuenta]):
                             valor = st.slider(f"{sector[:1]}-{entidad[:1]}-{cuenta}", 
                                                     min_value=0,
                                                     max_value=int(piv[2024][sector][entidad][cuenta]), 
